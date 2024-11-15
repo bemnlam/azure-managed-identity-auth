e7570d456c8f903ef72ec1bfe81f4c9a933cd91f
Ben Lam <5474099+bemnlam@users.noreply.github.com>
2024-11-15 07:50:36 -0600

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
 # azure-managed-identity-auth
-A minimal example showing how to protect an Azure app service using app registration and managed identity
+A minimal example showing how to protect an Azure app service using app registration and managed identity.
+
+The Caller app has an endpoint, `/weatherforcast`, which calls the Callee's API endpoint, `/weatherforcast`, and returns this API response.
+
+- Callee and Caller both require managed identity.
+- Callee also requires an App Registration.
+
+## Required tools
+
+- net8.0
+- [Azure Tools](https://marketplace.visualstudio.com/items?itemName=ms-vscode.vscode-node-azure-pack)
+
+## Up and Run
+
+### Install Dev Cert
+
+```bash
+dotnet dev-certs https --trust
+```
+
+### Callee
+
+```bash
+cd poc-callee
+dotnet restore
+dotnet build
+dotnet run --launch-profile https
+```
+
+The app will run at https://localhost:7200.
+
+### Caller
+
+```bash
+cd poc-caller
+dotnet restore
+dotnet build
+dotnet run --launch-profile https
+```
+
+The app will run at https://localhost:7122.
+
+## Azure configurations
+
+### App Registration (Callee only)
+- Create a App Registration named `poc-web-callee`
+- The Client (Application) ID will be used when setting up Caller
+
+### Caller
+
+* Create an Azure Web app named `poc-web-calleer`, which belongs to a newly created resource group, `poc-web-caller_group`.
+* Turn on **system-assigned managed identity**.
+* The managed identity will be used when setting up Callee.
+* Configure Environment Variables:
+  * `CalleeApi`: Callee's app url e.g. https://app_name.app_region-01.azurewebsites.net
+  * `DefaultScope`: *api://{Callee's client ID}/.default* e.g. `api://ecee9ced-1ac9-4657-b3be-0034a962f670/.default`
+
+### Callee
+
+* Create an Azure Web app named `poc-web-callee`, which belongs to a newly created resource group, `poc-web-callee_group`.
+* Turn on **system-assigned managed identity**.
+* Add Microsoft as the Identity Provider in **Authentication** settings.
+  * Choose App Registration `poc-web-callee`
+  * `Current tenant - Single tenant`
+  * `Allow requests from any application (Not recommended)`
+  * `Allow requests from specific identities`
+    * Fill in the Caller's managed identity ID
+  * Return 401 instead of 302
+
+## Expected Results
+
+- `poc-callee` should be able to deploy and run on `poc-web-callee`
+- `poc-caller` should be able to deploy and run on `poc-web-caller`
+- invoke `GET /token` in `poc-caller` should returns an access token
+- invoke `GET /weatherforecast` in `poc-caller` should returns a list of data
+
+---
+
+## References
+- https://learn.microsoft.com/en-us/azure/app-service/scenario-secure-app-authentication-app-service?tabs=workforce-configuration
+- https://learn.microsoft.com/en-us/azure/app-service/configure-authentication-provider-aad?tabs=workforce-configuration#use-a-built-in-authorization-policy
+- https://learn.microsoft.com/en-us/aspnet/core/tutorials/publish-to-azure-webapp-using-vscode?view=aspnetcore-8.0
---
+++ poc-caller/Program.cs
@@ -0,0 +1,98 @@
+using System.IdentityModel.Tokens.Jwt;
+using Azure.Core;
+using Azure.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Azure
+builder.Services.AddSingleton<TokenCredential>(ServiceProvider => {
+    return new ChainedTokenCredential(
+        new ManagedIdentityCredential(),
+        new EnvironmentCredential()
+    );
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+    app.UseSwagger();
+    app.UseSwaggerUI(options => {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "poc-caller v1");
+        options.RoutePrefix = string.Empty; // Set Swagger UI to root
+    });
+// }
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", async (IConfiguration configuration, TokenCredential credential, string? scope) =>
+{
+    var requestUri = $"{configuration["CalleeApi"]}/weatherforecast";
+    AccessToken token = default;
+    try{
+        token = await TokenHelper.GetToken(configuration, credential, scope);
+
+        using var httpClient = new HttpClient();
+        if(!string.IsNullOrEmpty(token.Token)) {
+            httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token.Token);
+        }
+
+        var data = await httpClient.GetFromJsonAsync<IEnumerable<WeatherForecast>>(requestUri);
+        return new ApiResponse(data ?? [], requestUri, token.Token, null);
+    }
+    catch(Exception ex) {
+          return new ApiResponse([], requestUri, token.Token, ex.Message);
+    }
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.MapGet("/token", async (IConfiguration configuration, TokenCredential credential, string? scope) => 
+{
+    AccessToken token = default;
+    try{
+        token = await TokenHelper.GetToken(configuration, credential, scope);
+        return new ApiResponse([], string.Empty, token.Token, string.Empty);
+    } catch(Exception ex) {
+        return new ApiResponse([], string.Empty, token.Token, ex.Message);
+    }
+})
+.WithName("GetToken")
+.WithOpenApi();
+
+app.MapGet("/ping", () => {
+    return new {status = "pong"};
+}).WithName("GetPing")
+.WithOpenApi();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+record ApiResponse(IEnumerable<WeatherForecast> data, string requestUri, string? token, string? ex) {
+    public JwtSecurityToken? jwt => string.IsNullOrEmpty(token) ? default : new JwtSecurityTokenHandler().ReadJwtToken(token);
+}
+
+public static class TokenHelper
+{
+    public static async Task<AccessToken> GetToken(IConfiguration configuration, TokenCredential credential, string? scope)
+    {
+        return await credential.GetTokenAsync(new TokenRequestContext(scopes: new[] { scope ?? configuration["DefaultScope"] }), new CancellationTokenSource().Token);
+
+    }
+}
